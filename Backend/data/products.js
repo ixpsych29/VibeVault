019f3a776f439fb1481e962c5abdcabc3b5633c8
malang <abdullahbinrafique945@gmail.com>
2024-05-28 22:04:17 +0500

    intial commit from backend

---
+++ Backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Amazon Echo Dot 3rd Generation",
+    image: "/images/alexa.jpg",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 29.99,
+    countInStock: 0,
+    rating: 3.2,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Google Nest Mini",
+    image: "/images/nest_mini.jpg",
+    description:
+      "Meet the 2nd generation Google Nest Mini, the smart speaker you control with your voice. Just say 'Hey Google' to play your favorite music.",
+    brand: "Google",
+    category: "Electronics",
+    price: 39.99,
+    countInStock: 15,
+    rating: 4.5,
+    numReviews: 24,
+  },
+  {
+    _id: "3",
+    name: "Apple HomePod Mini",
+    image: "/images/homepod_mini.jpg",
+    description:
+      "The Apple HomePod Mini delivers room-filling sound and features Siri intelligence and smart home control.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 99.99,
+    countInStock: 10,
+    rating: 2.7,
+    numReviews: 30,
+  },
+  {
+    _id: "4",
+    name: "Sonos One SL",
+    image: "/images/sonos_one_sl.jpg",
+    description:
+      "The powerful smart speaker with built-in voice control. Get rich, room-filling sound with Sonos One SL and control it with your voice.",
+    brand: "Sonos",
+    category: "Electronics",
+    price: 179.99,
+    countInStock: 5,
+    rating: 1,
+    numReviews: 18,
+  },
+  {
+    _id: "5",
+    name: "Bose SoundLink Micro",
+    image: "/images/bose_soundlink_micro.jpg",
+    description:
+      "The Bose SoundLink Micro is a portable Bluetooth speaker that delivers unmatched sound for its size. Rugged and waterproof design.",
+    brand: "Bose",
+    category: "Electronics",
+    price: 99.0,
+    countInStock: 8,
+    rating: 4.7,
+    numReviews: 45,
+  },
+  {
+    _id: "6",
+    name: "JBL Flip 5",
+    image: "/images/jbl_flip_5.jpg",
+    description:
+      "The JBL Flip 5 offers bold sound for every adventure. With its waterproof design and 12 hours of playtime, it’s perfect for outdoor fun.",
+    brand: "JBL",
+    category: "Electronics",
+    price: 119.95,
+    countInStock: 12,
+    rating: 0.5,
+    numReviews: 60,
+  },
+];
+
+export default products;
--- Frontend/src/screens/ProductScreen.jsx
+++ Frontend/src/screens/ProductScreen.jsx
@@ -6,7 +6,7 @@ import products from "../products";
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
-  const product = products.find((prod) => prod._id === productId);
+  const product = products.find((p) => p._id === productId);
   console.log(product);
 
   return (
@@ -15,64 +15,70 @@ const ProductScreen = () => {
         Go Back
       </Link>
 
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
+      {product ? (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
 
-        <Col md={4}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-            <ListGroup.Item>Price: ${product.description}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-
-        <Col md={3}>
-          <Card>
-            <ListGroup>
+          <Col md={4}>
+            <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
 
               <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    <strong>
-                      {product.countInStock > 0 ? "In Stock" : "Avaiable Soon"}
-                    </strong>
-                  </Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
               </ListGroup.Item>
-
-              <ListGroup.Item>
-                <Button
-                  type="button"
-                  className="btn-block"
-                  disabled={product.countInStock === 0}
-                >
-                  Add to Cart
-                </Button>
-              </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+              <ListGroup.Item>Price: ${product.description}</ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+
+          <Col md={3}>
+            <Card>
+              <ListGroup>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0
+                          ? "In Stock"
+                          : "Avaiable Soon"}
+                      </strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  <Button
+                    type="button"
+                    className="btn-block"
+                    disabled={product.countInStock === 0}
+                  >
+                    Add to Cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      ) : (
+        "Product not found"
+      )}
     </>
   );
 };
